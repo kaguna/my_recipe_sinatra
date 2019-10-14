0c0aaccab538208e0669aaa81f15a6a49b193613
kaguna <karyorkir@gmail.com>
2019-10-14 18:00:05 +0300

    Add category and recipe model specs

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ActiveRecord::Base
     has_many :recipes
     belongs_to :user
+
+    validates :name,
+            presence: { message: 'is a required field' },
+            uniqueness: { case_sensitive: false, message: 'has already been taken' },
+            length: { in: 3..20, message: 'is either too short or too long' }
 end
