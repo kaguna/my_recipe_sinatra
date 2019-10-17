ece621316dbb92345f4d5bd929838fde7ce4b9f9
kaguna <karyorkir@gmail.com>
2019-10-17 15:38:22 +0300

    Edit queue name

--- workers/email_sender_middleware.rb
+++ workers/email_sender_middleware.rb
@@ -3,6 +3,6 @@ require_relative '../config/pony'
 class EmailSender
     def self.email_sender(user)
         body = 'Hi You'
-        Pony.mail(to: user.email, subject: 'Registration successful', html_body: body)
+        Pony.mail(to: user.email, subject: 'Registration successful', body: body)
     end
 end
